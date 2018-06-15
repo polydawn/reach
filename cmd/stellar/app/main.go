a8ab20da40fdbe2e6ffd8d4eb5bcb8a6ce79e5e9
Eric Myhre <hash@exultant.us>
2018-06-15 17:14:22 +0200

    fiddly bits of subcommands and exitcodes.
    perennially.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cmd/stellar/app/main.go
+++ cmd/stellar/app/main.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
 
+	"github.com/polydawn/refmt"
+	"github.com/polydawn/refmt/json"
 	"github.com/urfave/cli"
+	"go.polydawn.net/go-timeless-api"
 )
 
-func Main(ctx context.Context, args []string, stdin io.Reader, stdout, stderr io.Writer) int {
+func Main(ctx context.Context, args []string, stdin io.Reader, stdout, stderr io.Writer) (exitCode int) {
 	app := &cli.App{
 		Name:      "stellar",
 		Usage:     "sidereal repeatr",
@@ -19,14 +23,25 @@ func Main(ctx context.Context, args []string, stdin io.Reader, stdout, stderr io
 				Name:  "emerge",
 				Usage: "todo docs",
 				Action: func(ctx *cli.Context) error {
+					mod := api.Module{}
+					unm := refmt.NewUnmarshallerAtlased(json.DecodeOptions{}, os.Stdin, api.Atlas_Module)
+					err := unm.Unmarshal(&mod)
+					if err != nil {
+						panic(err)
+					}
 					return nil
 				},
 			},
 		},
+		// Must configure this to override an os.Exit(3).
+		CommandNotFound: func(ctx *cli.Context, command string) {
+			exitCode = 1
+			fmt.Fprintf(stderr, "stellar: incorrect usage: '%s' is not a %s subcommand\n", command, ctx.App.Name)
+		},
 	}
 	if err := app.Run(args); err != nil {
-		fmt.Fprintf(stderr, "Incorrect usage: %s", err)
-		return 1
+		exitCode = 1
+		fmt.Fprintf(stderr, "stellar: incorrect usage: %s", err)
 	}
-	return 0
+	return
 }
--- cmd/stellar/app/main_test.go
+++ cmd/stellar/app/main_test.go
@@ -15,7 +15,7 @@ func bufferedMain(args ...string) (int, string, string) {
 	return exitCode, stdout.String(), stderr.String()
 }
 
-func Test(t *testing.T) {
+func TestNoargsHelptext(t *testing.T) {
 	exitCode, stdout, stderr := bufferedMain("stellar")
 	Wish(t, exitCode, ShouldEqual, 0)
 	Wish(t, stdout, ShouldEqual, "")
@@ -34,4 +34,22 @@ func Test(t *testing.T) {
 		   --help, -h     show help
 		   --version, -v  print the version
 	`))
+}
+
+func TestWrongCommandHelptext(t *testing.T) {
+	exitCode, stdout, stderr := bufferedMain("stellar", "not a command")
+	Wish(t, exitCode, ShouldEqual, 1)
+	Wish(t, stdout, ShouldEqual, "")
+	Wish(t, stderr, ShouldEqual, Dedent(`
+		stellar: incorrect usage: 'not a command' is not a stellar subcommand
+	`))
+
+	t.Run("also when asking for help", func(t *testing.T) {
+		exitCode, stdout, stderr := bufferedMain("stellar", "not a command", "-h")
+		Wish(t, exitCode, ShouldEqual, 1)
+		Wish(t, stdout, ShouldEqual, "")
+		Wish(t, stderr, ShouldEqual, Dedent(`
+			stellar: incorrect usage: 'not a command' is not a stellar subcommand
+		`))
+	})
 }
