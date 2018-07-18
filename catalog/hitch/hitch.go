cc1946036240c21f78e702bfcf529c0bca80c2c7
Eric Myhre <hash@exultant.us>
2018-07-18 19:17:39 +0200

    Fixup to fully use that shared catalog code.
    I thought I already staged this -.- too impatient to rebase around an mv.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- catalog/hitch/hitch.go
+++ catalog/hitch/hitch.go
@@ -4,16 +4,8 @@ package hitch
 
 import (
 	"context"
-	"fmt"
-	"os"
-	"path/filepath"
-
-	"github.com/polydawn/go-errcat"
-	"github.com/polydawn/refmt"
-	"github.com/polydawn/refmt/json"
 
 	"go.polydawn.net/go-timeless-api"
-	"go.polydawn.net/go-timeless-api/funcs"
 	"go.polydawn.net/go-timeless-api/hitch"
 	"go.polydawn.net/stellar/catalog"
 )
@@ -42,31 +34,9 @@ func (cat FSCatalog) ViewWarehouses(
 	ctx context.Context,
 	modName api.ModuleName,
 ) (ws *api.WareSourcing, err error) {
-	funcs.MustValidate(modName)
-
-	modPath := filepath.Join(cat.Root, string(modName))
-	if fi, err := os.Stat(modPath); err != nil || !fi.IsDir() {
-		return nil, errcat.Errorf(hitch.ErrNoSuchCatalog, "module not found: %q is not a dir", modName)
-	}
-	mirrorFilePath := filepath.Join(modPath, "mirrors.tl")
-	if fi, err := os.Stat(mirrorFilePath); err != nil || !(fi.Mode()&os.ModeType == 0) {
+	ws, err = catalog.Tree{cat.Root}.LoadModuleMirrors(modName)
+	if ws == nil {
 		return &api.WareSourcing{}, nil
 	}
-	f, err := os.Open(mirrorFilePath)
-	if err != nil {
-		return nil, errcat.ErrorDetailed(hitch.ErrCorruptState, fmt.Sprintf("module mirrors list for %q cannot be opened: %s", api.ItemRef{modName, "", ""}.String(), err),
-			map[string]string{
-				"ref": api.ItemRef{modName, "", ""}.String(),
-			})
-	}
-	defer f.Close()
-	err = refmt.NewUnmarshallerAtlased(json.DecodeOptions{}, f, api.Atlas_WareSourcing).Unmarshal(&ws)
-	if err != nil {
-		return nil, errcat.ErrorDetailed(hitch.ErrCorruptState, fmt.Sprintf("module mirrors list for %q cannot be opened: %s", api.ItemRef{modName, "", ""}.String(), err),
-			map[string]string{
-				"ref": api.ItemRef{modName, "", ""}.String(),
-			})
-	}
 	return
-
 }
