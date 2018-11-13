ba39debbabce1a1d244c5bbac021a5c96c55b651
Eric Myhre <hash@exultant.us>
2018-11-13 13:33:53 -0600

    Prepare some helper funcs for isolating test cwd.
    Should've done this ages ago; time to scratch it off the list.
    
    Pulled in a rio link to do it.  Could've certainly generated this code
    again without pulling in a dependency; but seems unproductive to bother
    when we've already got all that lovely logic for handling symlinks and
    everything else under the sun.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ examples/testutil/withTempdir.go
@@ -0,0 +1,56 @@
+package testutil
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+
+	"go.polydawn.net/rio/fs"
+	"go.polydawn.net/rio/fs/osfs"
+	"go.polydawn.net/rio/fsOp"
+)
+
+func WithTmpdir(fn func(tmpDir fs.AbsolutePath)) {
+	tmpBase := "/tmp/stellar-test/"
+	err := os.MkdirAll(tmpBase, os.FileMode(0777)|os.ModeSticky)
+	if err != nil {
+		panic(err)
+	}
+
+	tmpdir, err := ioutil.TempDir(tmpBase, "")
+	if err != nil {
+		panic(err)
+	}
+
+	defer os.RemoveAll(tmpdir)
+	fn(fs.MustAbsolutePath(tmpdir))
+}
+
+func WithClonedTmpdir(src fs.AbsolutePath, fn func(tmpDir fs.AbsolutePath)) {
+	WithTmpdir(func(tmpDir fs.AbsolutePath) {
+		if err := fsOp.Copy(context.Background(), osfs.New(src), osfs.New(tmpDir)); err != nil {
+			panic(err)
+		}
+		fn(tmpDir)
+	})
+}
+
+func WithCwdClonedTmpDir(src fs.AbsolutePath, fn func()) {
+	WithClonedTmpdir(src, func(tmpDir fs.AbsolutePath) {
+		wd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+		os.Chdir(tmpDir.String())
+		defer os.Chdir(wd)
+		fn()
+	})
+}
+
+func GetCwdAbs() fs.AbsolutePath {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return fs.MustAbsolutePath(wd)
+}
